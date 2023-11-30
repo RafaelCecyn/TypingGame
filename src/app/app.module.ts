f33fa9cd1338f0259d564e81cae4852310078aec
Rafael Cecyn <rafael.mendes112@hotmail.com>
2023-11-30 16:05:39 -0300

    Jogo de digitacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CadastroComponent } from './components/cadastro/cadastro.component';
+import { PagesModule } from './pages/pages.module';
+import { RoutingModule } from './pages/routing.module';
+import { RouterModule, Routes } from '@angular/router';
+// import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    PagesModule,
+    RoutingModule,
+    RouterModule,
+    // HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
